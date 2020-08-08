37940c3276fd6564fbad344e1102a4609306ea4d
gossdouglas <gossdouglas@yahoo.com>
2020-08-08 10:06:50 -0500

    can see all records now.

--- Controllers/dailyReportAllController.cs
+++ Controllers/dailyReportAllController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using allpax_service_record.Models;
+using allpax_service_record.Models.View_Models;
 
 namespace allpax_service_record.Controllers
 {
@@ -18,9 +19,23 @@ namespace allpax_service_record.Controllers
         public ActionResult Index()
         {
             //return View(db.tbl_customers.ToList());
-            var sql = db.tbl_dailyReport.SqlQuery("SELECT * from tbl_dailyReport").ToList();
-           
-            return View(sql.ToList()); 
+            //var sql = db.tbl_dailyReport.SqlQuery("SELECT * from tbl_dailyReport").ToList();
+            List<vm_dailyReportViewAll> list  = db.Database.SqlQuery<vm_dailyReportViewAll>("SELECT tbl_dailyReport.dailyReportID, tbl_Jobs.active, tbl_dailyReport.date, tbl_dailyReport.jobID, " +
+                "tbl_subJobTypes.description, tbl_customers.customerName, tbl_customers.address FROM tbl_dailyReport " +
+
+                    "INNER JOIN " +
+                    "tbl_Jobs ON tbl_Jobs.jobID = tbl_dailyReport.jobID " +
+                    "INNER JOIN " +
+                    "tbl_customers ON tbl_customers.customerCode = tbl_Jobs.customerCode " +
+                    "INNER JOIN " +
+                    "tbl_jobSubJobs ON tbl_jobSubJobs.jobID = tbl_Jobs.jobID " +
+                    "INNER JOIN " +
+                    "tbl_subJobTypes ON tbl_subJobTypes.subJobID = tbl_jobSubJobs.subJobID " +
+                    "WHERE " +
+                    "tbl_dailyReport.subJobID = tbl_subJobTypes.subJobID").ToList();
+
+
+            return View(list.ToList()); 
         }
 
         //begin CMPS 411 controller code
---
+++ Models/View Models/vm_dailyReportViewAll.cs
@@ -0,0 +1,34 @@
+namespace allpax_service_record.Models.View_Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class vm_dailyReportViewAll
+    {
+        public int dailyReportID { get; set; }
+
+        public Boolean active { get; set; }
+
+        public DateTime date { get; set; }
+
+        public string jobID { get; set; }
+
+        //public string subJobDescription { get; set; }
+
+        public string description { get; set; }
+
+        public string customerName { get; set; }
+
+        public string location { get; set; }
+
+        public int subJobID { get; set; }
+
+        public string team { get; set; }
+
+        public string workDescription { get; set; }    
+       
+    }
+}
