421d13caa6b616c4f081e9925ba9a8a87055bb3f
gossdouglas <gossdouglas@yahoo.com>
2020-08-11 10:30:16 -0500

    loading up with the sql object.

--- Controllers/dailyReportByReportIDController.cs
+++ Controllers/dailyReportByReportIDController.cs
@@ -7,6 +7,10 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using allpax_service_record.Models;
+using allpax_service_record.Models.View_Models;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Dynamic;
 
 namespace allpax_service_record.Controllers
 {
@@ -17,11 +21,63 @@ namespace allpax_service_record.Controllers
         // GET: customers
         public ActionResult Index(string reportID)
         {
-            ViewBag.reportID = reportID;
-            //return View(db.tbl_customers.ToList());
-            var sql = db.tbl_dailyReport.SqlQuery("SELECT * from tbl_dailyReport").ToList();
-           
-            return View(sql.ToList()); 
+            //ViewBag.reportID = reportID;           
+
+            List<vm_dailyReportByReportID> dailyReportByID = new List<vm_dailyReportByReportID>();
+            string mainconn = ConfigurationManager.ConnectionStrings["allpaxServiceRecordEntities"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+
+            sqlconn.Open();
+
+            //begin query for kits available, but not installed
+            string sqlquery1 =
+                "SELECT tbl_dailyReport.dailyReportID, tbl_dailyReport.jobID, tbl_subJobTypes.description, tbl_dailyReport.date, " +
+                "tbl_Jobs.customerContact,tbl_customers.customerName, tbl_customers.address, tbl_dailyReport.equipment, " +
+                "tbl_dailyReport.startTime, tbl_dailyReport.endTime, tbl_dailyReport.lunchHours " +
+
+                "FROM tbl_dailyReport " +
+
+                "INNER JOIN " +
+                "tbl_Jobs ON tbl_Jobs.jobID = tbl_dailyReport.jobID " +
+                "INNER JOIN " +
+                "tbl_customers ON tbl_customers.customerCode = tbl_Jobs.customerCode " +
+                "INNER JOIN " +
+                "tbl_jobSubJobs ON tbl_jobSubJobs.jobID = tbl_Jobs.jobID " +
+                "INNER JOIN " +
+                "tbl_subJobTypes ON tbl_subJobTypes.subJobID = tbl_jobSubJobs.subJobID " +
+
+                "WHERE " +
+
+                "tbl_dailyReport.subJobID = tbl_subJobTypes.subJobID " +
+                "AND " +
+                "tbl_dailyReport.dailyReportID LIKE @reportID";
+
+            SqlCommand sqlcomm1 = new SqlCommand(sqlquery1, sqlconn);
+            sqlcomm1.Parameters.AddWithValue("@reportID", reportID);
+            SqlDataAdapter sda1 = new SqlDataAdapter(sqlcomm1);
+            DataTable dt1 = new DataTable();
+            sda1.Fill(dt1);
+            foreach (DataRow dr1 in dt1.Rows)
+            {
+                vm_dailyReportByReportID vm_dailyReportByReportID = new vm_dailyReportByReportID();
+
+                vm_dailyReportByReportID.dailyReportID = (int)dr1[0];
+                vm_dailyReportByReportID.jobID = dr1[1].ToString();
+                vm_dailyReportByReportID.description = dr1[2].ToString();
+                vm_dailyReportByReportID.date = Convert.ToDateTime(dr1[3]);
+                vm_dailyReportByReportID.customerContact = dr1[4].ToString();
+                vm_dailyReportByReportID.customerName = dr1[5].ToString();
+                vm_dailyReportByReportID.address = dr1[6].ToString();
+                vm_dailyReportByReportID.equipment = dr1[7].ToString();
+                vm_dailyReportByReportID.startTime = Convert.ToDateTime(dr1[8]);
+                vm_dailyReportByReportID.endTime = Convert.ToDateTime(dr1[9]);
+
+                dailyReportByID.Add(vm_dailyReportByReportID);
+            }
+
+            sqlconn.Close();
+            //end query
+            return View(dailyReportByID);
         }
 
         //begin CMPS 411 controller code
---
+++ Models/View Models/vm_dailyReportByReportID.cs
@@ -0,0 +1,40 @@
+namespace allpax_service_record.Models.View_Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class vm_dailyReportByReportID
+    {
+        public int dailyReportID { get; set; }
+
+        public Boolean active { get; set; }
+
+        public DateTime date { get; set; }
+        public DateTime startTime { get; set; }
+        public DateTime endTime { get; set; }
+
+
+        public string jobID { get; set; }
+
+        //public string subJobDescription { get; set; }
+
+        public string description { get; set; }
+        public string equipment { get; set; }
+
+        public string customerName { get; set; }
+
+        public string customerContact { get; set; }
+
+        public string address { get; set; }
+
+        public int subJobID { get; set; }
+
+        public string team { get; set; }
+
+        public string workDescription { get; set; }    
+       
+    }
+}
